9b532f5cd052bf203ea82c6e1939f0fb3d6abfff
Talgat Manglayev <talgat.manglayev@gmail.com>
2024-08-16 21:12:38 +0500

    updated CSCI-111 week-1

--- CSCI-111/week-1/lab/tutorial-1/main.c
+++ CSCI-111/week-1/lab/tutorial-1/main.c
@@ -4,8 +4,9 @@ int main()
     //ASCII standard
     char letterA = 'A';
     char letterB = 66;
-
+    char letterC = 'A'+2;
     printf("%c\n", letterA);
     printf("%c\n", letterB);
+    printf("%c\n", letterC);
     return 0;
 }
