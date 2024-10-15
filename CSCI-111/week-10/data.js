26fc0b929d001a4775c0cb16faa34e58ec1dfff1
Talgat Manglayev <talgat.manglayev@gmail.com>
2024-10-15 16:56:36 +0500

    CSCI 111 added quiz material

---
+++ CSCI-111/week-10/data.js
@@ -0,0 +1,19 @@
+students = [{
+    "firstname": "STUDENTNAME1",
+    "lastname": "STUDENTLASTNAME1",
+    "idnumber": "123456788",
+    "email": "firstname.lastname@nu.edu.kz",
+    "department": "Political Science and International Relations",
+    "institution": "School of Sciences and Humanities",
+    "question1": "Draw flag of Armenia using SVG",
+    "question2": "Declare array of 10 random elements; Find Maximum of first 5 elements; Example array: [5, 2, 0, 4, 7, 9, 8, 1, -9, 2]. Result: 7"
+},{
+    "firstname": "STUDENTNAME2",
+    "lastname": "STUDENTLASTNAME2",
+    "idnumber": "123456789",
+    "email": "firstname.lastname@nu.edu.kz",
+    "department": "Political Science and International Relations",
+    "institution": "School of Sciences and Humanities",
+    "question1": "Draw flag of Bahamas using SVG",
+    "question2": "Declare array of 10 random elements; Find Maximum of last 5 elements; Example array: [5, 2, 0, 4, 7, 9, 8, 1, -9, 2]. Result: 9"
+}]
