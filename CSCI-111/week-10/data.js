8be7a712d745b5b561d9bf61afc3e7a2cc3c4bab
Talgat Manglayev <talgat.manglayev@gmail.com>
2024-10-17 19:36:42 +0500

    added material

--- CSCI-111/week-10/data.js
+++ CSCI-111/week-10/data.js
@@ -1,19 +1,33 @@
-students = [{
-    "firstname": "STUDENTNAME1",
-    "lastname": "STUDENTLASTNAME1",
-    "idnumber": "123456788",
-    "email": "firstname.lastname@nu.edu.kz",
-    "department": "Political Science and International Relations",
-    "institution": "School of Sciences and Humanities",
-    "question1": "Draw flag of Armenia using SVG",
-    "question2": "Declare array of 10 random elements; Find Maximum of first 5 elements; Example array: [5, 2, 0, 4, 7, 9, 8, 1, -9, 2]. Result: 7"
-},{
-    "firstname": "STUDENTNAME2",
-    "lastname": "STUDENTLASTNAME2",
-    "idnumber": "123456789",
-    "email": "firstname.lastname@nu.edu.kz",
-    "department": "Political Science and International Relations",
-    "institution": "School of Sciences and Humanities",
-    "question1": "Draw flag of Bahamas using SVG",
-    "question2": "Declare array of 10 random elements; Find Maximum of last 5 elements; Example array: [5, 2, 0, 4, 7, 9, 8, 1, -9, 2]. Result: 9"
-}]
+students =
+[
+    {
+        "firstname": "Aigerim",
+        "lastname": "Ibrayeva",
+        "idnumber": "123456788",
+        "email": "firstname.lastname@nu.edu.kz",
+        "department": "Political Science and International Relations",
+        "institution": "School of Sciences and Humanities",
+        "question1": "Draw flag of Armenia using SVG",
+        "question2": "Declare array of 10 random elements; Find Maximum of first 5 elements; Example array: [5, 2, 0, 4, 7, 9, 8, 1, -9, 2]. Result: 7"
+    },
+    {
+        "firstname": "Batyr",
+        "lastname": "Abdalimov",
+        "idnumber": "123456789",
+        "email": "firstname.lastname@nu.edu.kz",
+        "department": "History",
+        "institution": "School of Sciences and Humanities",
+        "question1": "Draw flag of Bahamas using SVG",
+        "question2": "Declare array of 10 random elements; Find Maximum of last 5 elements; Example array: [5, 2, 0, 4, 7, 9, 8, 1, -9, 2]. Result: 9"
+    },
+    {
+        "firstname": "Edil",
+        "lastname": "Bekov",
+        "idnumber": "123456790",
+        "email": "firstname.lastname@nu.edu.kz",
+        "department": "Economics",
+        "institution": "School of Sciences and Humanities",
+        "question1": "Draw flag of Russian Federation using SVG",
+        "question2": "Declare array of 10 random elements; Find Minimum of last 5 elements; Example array: [5, 2, 0, 4, 7, 9, 8, 1, -9, 2]. Result: -9"
+    }
+]
