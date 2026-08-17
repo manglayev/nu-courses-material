bb2a57ba0d4e920fcaa444bc633938d31482cdb3
Talgat-qypshaq <talgat.manglayev@gmail.com>
2026-08-17 16:23:47 +0500

    minor updates

--- CSCI-423/week-6/Lecture_6/C++_Examples/Example_8/main.cu
+++ CSCI-423/week-6/Lecture_6/C++_Examples/Example_8/main.cu
@@ -8,6 +8,15 @@
 #define THREADS 1
 #define BLOCKS 1
 
+#define CHECK_CUDA_ERROR(call) \
+do { \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+        fprintf(stderr, "CUDA Error: %s\n", cudaGetErrorString(err)); \
+        exit(1); \
+    } \
+} while(0)
+
 __host__ __device__ int customFunction(int a, int *b)
 {
   return a + a;
@@ -15,8 +24,8 @@ __host__ __device__ int customFunction(int a, int *b)
 
 __global__ void globalFunction(int a, int *b)
 {
-  int thread = threadIdx.x + blockIdx.x*blockDim.x;
-  if(thread < THREADS)
+  int thread = threadIdx.x + blockIdx.x * blockDim.x;
+  if(thread < BLOCKS * THREADS)
   {
     b[0] = customFunction(a, b) + a;
   }
@@ -26,12 +35,12 @@ int main()
 {
   int a = 4;
   int *b;
-  cudaMallocManaged(&b, sizeof(int));
+  CHECK_CUDA_ERROR(cudaMallocManaged(&b, sizeof(int)));
   b[0] = customFunction(a, b);
   printf("b 1 = %d;\n", b[0]);
   globalFunction<<<BLOCKS, THREADS>>>(a, b);
-  cudaDeviceSynchronize();
+  CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   printf("b 2 = %d;\n", b[0]);
-  cudaFree(b);
+  CHECK_CUDA_ERROR(cudaFree(b));
   return 0;
 }
