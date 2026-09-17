62bbc2e496d83e1aa28e4556032d03315e7ca62d
Talgat-qypshaq <talgat.manglayev@gmail.com>
2026-09-17 21:39:16 +0500

    improved CSCI-151 week-5 lesson-2

--- CSCI-151/week-5/lesson-2/example-2.c
+++ CSCI-151/week-5/lesson-2/example-2.c
@@ -3,11 +3,12 @@ example-2
 Create a file encryption processor (from decimal into binary) using 
 getc(), putc(), rename(), and stderr error reporting.
 Requirements:
- - Open “source.txt” for reading (“r”) and “encrypted.txt” for writing (“w”). Validate pointers against NULL.
- - Read characters using getc(in) until EOF: ‘0’ - ‘9’
+ - Open “source.txt” for reading (“r”) and “encrypted.txt” for writing (“w”). 
+ Validate pointers against NULL.
+ - Read a character using getc(in) until EOF: '0' - '9'
  - Write all characters (transformed and untransformed) to “encrypted.txt” using putc(c, out).
  - Close both files.
- - Use rename (“encrypted.txt”, “secure_vault.txt”) to rename the output file.
+ - Use rename ("encrypted.txt", "secure_vault.txt") to rename the output file.
  If rename() returns non-zero, write an error to stderr and exit with exit (EXIT_FAILURE).
 */
 #include <stdio.h>
@@ -19,44 +20,63 @@ int main()
 	FILE *outfile;
 
 	infile = fopen("source.txt", "r");
-	outfile = fopen("encrypt.txt", "w");
+	outfile = fopen("encrypted.txt", "w");
 
 	if(infile == NULL || outfile == NULL)
 	{
-		printf("Problem opening the file.\n");
-		return 1;
+		fprintf(stderr, "Error. Failed to open files\n");
+		if (infile) fclose(infile);
+        if (outfile) fclose(outfile);
+		exit(EXIT_FAILURE);
 	}
 
 	printf("Files opened succesfully\n");
-	char decimalChar, binaryChar;
-	int decimalInt, binaryInt;
-	while((decimalChar = getc(infile)) != EOF)
+	int decimalCharASCII, decimalInt;
+	/*
+	int decimalCharASCII = getc(infile);
+	gives us the integer value of the character read from the file.
+	*/
+	while((decimalCharASCII = getc(infile)) != EOF)
 	{
-		printf("infile character:%c\n", decimalChar);
-		decimalInt = decimalChar - '0';
-		printf("decimalInt %d\n", decimalInt);
-		do
+		printf("ASCII value %d; character:%c\n", decimalCharASCII, decimalCharASCII);		
+		if (decimalCharASCII >= '0' && decimalCharASCII <= '9')
 		{
-			binaryInt = decimalInt % 2;
-			//printf("binaryInt %d\n", binaryInt);
-			binaryChar = binaryInt + '0';
+			decimalInt = decimalCharASCII - '0';
+
+			int b0 = decimalInt % 2;
+    		decimalInt = decimalInt / 2;
+			
+			int b1 = decimalInt % 2;
 			decimalInt = decimalInt / 2;
-			putc(binaryChar, outfile);
-		}while(decimalInt > 0 );
+
+			int b2 = decimalInt % 2;
+			decimalInt = decimalInt / 2;
+
+			int b3 = decimalInt % 2;
+
+			putc(b3 + '0', outfile);
+			putc(b2 + '0', outfile);
+			putc(b1 + '0', outfile);
+			putc(b0 + '0', outfile);
+		}
+		else
+		{
+			putc(decimalCharASCII + '0', outfile);
+		}
 	}
-	putc('\n',outfile);
-	if(rename("encrypt.txt", "secure_vault.txt") == 0)
+
+	fclose(outfile);
+	fclose(infile);
+
+	if(rename("encrypted.txt", "secure_vault.txt") == 0)
 	{
-		printf("rename successfuly\n");
+		printf("File encrypted.txt was renamed into secure_vault.txt successfully\n");
 	}
 	else
 	{
 		fprintf(stderr, "Error. Failed to rename\n");
 		exit(EXIT_FAILURE);
 	}
-
-	fclose(outfile);
-	fclose(infile);
 	
 	return 0;
 }
